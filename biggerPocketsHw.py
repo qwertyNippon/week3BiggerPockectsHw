633fba9dba6bf36b5c74bc2c0183f15e8d5f2b99
qwertyNippon <orlandohenry100@gmail.com>
2023-06-04 13:35:54 -0400

    making app

---
+++ biggerPocketsHw.py
@@ -0,0 +1,26 @@
+
+class ROI_Cal():
+    """
+    Project to use in the future.
+    total income, total expenses, total invested
+    *** total income and total expenses --- on a month to month basis***
+    ***cash flow --- income minus expenses***
+    *** total invested --- how much you paid to acquire the property.***
+    ***annual cash flow --- cash flow on a yearly basis***
+    """   
+    def __init__(self, total_income, total_expenses, total_invested):
+        self.total_income = total_income
+        self.total_expenses = total_expenses
+        self.total_invested = total_invested
+
+    def income_cal(self):
+        inc = input(f"input your total revenue that may have been forgotten above, on a monthly basis for one or more properties. ")
+        self.total_income = inc + self.total_income
+        print(self.total_income)
+
+    def exp_cal(self):
+        exp = input(f"input your total expenses that may have been forgotten above, on a monthly basis for one or more properties. ")
+        self.total_expenses = exp + self.total_expenses
+        print(self.total_expenses)
+
+ROI_Cal(2000, 1550, 50000)
